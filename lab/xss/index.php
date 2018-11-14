7509f3474ca5329f808ce827e33d2087ffb4dbd6
Pablo Umanzor A <info@redlibre.cl>
2018-11-14 03:21:02 -0300

    Create index.php

---
+++ lab/xss/index.php
@@ -0,0 +1,25 @@
+<html><head><title>Login</title>
+
+<body bgcolor=white link=blue alink=CC9966 vlink=purple>
+<font color=black>
+<center>
+<table>
+<table border=0 width=100%>
+<tr><th bgcolor=#608080 align=center><font color=white><font size=4>Login</font></th></tr>
+<tr><td>&nbsp</tr></td>
+</table>
+<table border=0>
+<form action="login.php" method="POST" name="form1">
+       <tr><th align=left>Informacion 
+       <tr><td align=right><font color=red>*</font>username <input type="text" name="username" VALUE="" size=11>
+       <td align=right><font color=red>*</font>password<input type="text" name="password" VALUE="">
+       
+       <td align=right>
+       <tr><td align=right>
+       <tr><td>&nbsp
+       <tr><td>&nbsp
+       <tr><th align=left>
+       <tr><th colspan=3 align=center><font color=#c06080><input type=submit value='Ingresar'></font>
+       </form>
+       </table>
+</center></html>
